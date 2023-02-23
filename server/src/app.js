ca779c590aa2cced1c626e2733e47705bcacee87
Kai Lew <79333361+kailew@users.noreply.github.com>
2023-02-23 12:27:09 -0800

    adding in new routers

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router')
 
 const app = express();
 
@@ -9,6 +11,11 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,9 @@
+const { launches } = require('../../modles/launches.model');
+
+function getAllLaunches(req, res) {
+    return res.status(200).json(Array.from(launches.values()));
+}
+
+module.exports = {
+    getAllLaunches
+}
