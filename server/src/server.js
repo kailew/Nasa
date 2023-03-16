4eb8c342bcbe8aa9d781bf4f05460cb36e8f3c58
Kai Lew <79333361+kailew@users.noreply.github.com>
2023-03-16 11:06:12 -0700

    mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,8 @@ const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
+const MONGO_URL = 'mongodb+srv://kailew2020:c5KnEK9wzFKgYVmi@cluster0.aihykfu.mongodb.net/?retryWrites=true&w=majority'
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
